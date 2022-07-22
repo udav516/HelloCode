93debd598cb6e3c53a9aee2475cb966ca16da371
udav516 <eldar.udav516@yandex.ru>
2022-07-22 16:21:22 +0300

    Example 010 Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {15, 22, 34, 4, 81, 75, 62, 57, 81, 96};
+
+int n = array.Length;
+int find = 81;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index +1;
+    index++;
+}
+
